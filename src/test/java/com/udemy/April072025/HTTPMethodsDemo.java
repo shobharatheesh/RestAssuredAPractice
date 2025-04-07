81afa66d995019be6b8473f4b485d2caaaacd239
Shobha Rao <sraork@gmail.com>
2025-04-07 18:01:19 +0530

    HTTP methods--7th March 2025

--- src/test/java/com/udemy/April072025/HTTPMethodsDemo.java
+++ src/test/java/com/udemy/April072025/HTTPMethodsDemo.java
@@ -2,12 +2,14 @@ package com.udemy.April072025;
 
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class HTTPMethodsDemo {
 
-    //Test to retrieve the users and validate the response
+    //1. GET ---> Test to retrieve the users and validate the response
 
     @Test(priority = 1)
 
@@ -23,10 +25,37 @@ public class HTTPMethodsDemo {
                         .header("Content-Type",equalTo("application/json; charset=utf-8"))
                         .time(lessThan(2000L))
                         .log().all();
+    }
+
+    //2.POST creating a new user and validating
+
+    @Test(priority = 2)
+    void createUsers()
+    {
+
+        HashMap<String, String> data = new HashMap<String,String>();
+        data.put("name","pavan");
+        data.put("job","trainer");
+
+        given()
+                .contentType("application/json")
+                .body(data)
+        .when()
+                .post("https://reqres.in/api/users")
+        .then()
+                .statusCode(201)
+                .header("Content-Type",equalTo("application/json; charset=utf-8"))
+                .time(lessThan(2000L))
+                .body("name",equalTo("pavan"))
+                .body("job",equalTo("trainer"))
+                .body(containsString("id"))
+                .log().all();
+
+
+
 
 
 
     }
-
 
 }
