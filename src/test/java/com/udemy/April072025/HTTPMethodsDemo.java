57860ce0f1d93ca75d80658595fea68ccacbcf28
Shobha Rao <sraork@gmail.com>
2025-04-07 15:36:14 +0530

    HTTP methods--7th March 2025

---
+++ src/test/java/com/udemy/April072025/HTTPMethodsDemo.java
@@ -0,0 +1,31 @@
+package com.udemy.April072025;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class HTTPMethodsDemo {
+
+    //Test to retrieve the users and validate the response
+
+    @Test(priority = 1)
+
+    void getUsers()
+    {
+                given()
+                .when()
+                      .get("https://reqres.in/api/users?page=2")
+                .then()
+                        .statusCode(200)
+                        .body("page",equalTo(2))
+                        .body(containsString("email"))
+                        .header("Content-Type",equalTo("application/json; charset=utf-8"))
+                        .time(lessThan(2000L))
+                        .log().all();
+
+
+    }
+
+
+}
