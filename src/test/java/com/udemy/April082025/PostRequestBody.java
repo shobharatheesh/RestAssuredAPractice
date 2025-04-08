ce43abd49bd7072fb6d84280eda631e64173d723
Shobha Rao <sraork@gmail.com>
2025-04-08 17:43:17 +0530

    HTTP methods--8th March 2025

---
+++ src/test/java/com/udemy/April082025/PostRequestBody.java
@@ -0,0 +1,60 @@
+package com.udemy.April082025;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostRequestBody {
+
+    String studentId;
+
+    //1.create post request body using  HashMap
+
+    @Test
+    void createStudentsusingHashMap() {
+
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("name", "Scott");
+        data.put("location", "France");
+        data.put("phone", "123456");
+
+        String courses[] = {"C", "C++"};
+        data.put("courses", courses);
+
+
+        studentId = given()
+                .contentType("application/json")
+                .body(data)
+                .when()
+                .post("http://localhost:3000/students")
+
+                .then()
+                .statusCode(201)
+                .body("name", equalTo("Scott"))
+                .body("location", equalTo("France"))
+                .body("phone", equalTo("123456"))
+                .body("courses[0]", equalTo("C"))
+                .body("courses[1]", equalTo("C++"))
+                .header("Content-Type", equalTo("application/json"))
+                .log().all()
+                .extract().jsonPath().getString("id");
+
+    }
+
+    @AfterMethod
+    void deleteStudentRecord()
+    {
+        given()
+        .when()
+                .delete("http://localhost:3000/students/" + studentId)
+        .then()
+                .statusCode(200);
+
+    }
+
+    }
+
