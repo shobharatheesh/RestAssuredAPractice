21fb7524159c812b8a65eca3457d20406bffa0b8
Shobha Rao <sraork@gmail.com>
2025-04-08 11:11:29 +0530

    HTTP methods--8th March 2025

--- src/test/java/com/udemy/April072025/HTTPMethodsDemo.java
+++ src/test/java/com/udemy/April072025/HTTPMethodsDemo.java
@@ -9,53 +9,74 @@ import static org.hamcrest.Matchers.*;
 
 public class HTTPMethodsDemo {
 
+    int userId;
+
     //1. GET ---> Test to retrieve the users and validate the response
 
     @Test(priority = 1)
-
-    void getUsers()
-    {
-                given()
+    void getUsers() {
+        given()
                 .when()
-                      .get("https://reqres.in/api/users?page=2")
+                .get("https://reqres.in/api/users?page=2")
                 .then()
-                        .statusCode(200)
-                        .body("page",equalTo(2))
-                        .body(containsString("email"))
-                        .header("Content-Type",equalTo("application/json; charset=utf-8"))
-                        .time(lessThan(2000L))
-                        .log().all();
+                .statusCode(200)
+                .body("page", equalTo(2))
+                .body(containsString("email"))
+                .header("Content-Type", equalTo("application/json; charset=utf-8"))
+                .time(lessThan(2000L))
+                .log().all();
     }
 
     //2.POST creating a new user and validating
 
     @Test(priority = 2)
-    void createUsers()
-    {
+    void createUsers() {
 
-        HashMap<String, String> data = new HashMap<String,String>();
-        data.put("name","pavan");
-        data.put("job","trainer");
+        HashMap<String, String> data = new HashMap<String, String>();
+        data.put("name", "pavan");
+        data.put("job", "trainer");
+
+        userId = given()
+                .contentType("application/json")
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201)
+                .header("Content-Type", equalTo("application/json; charset=utf-8"))
+                .time(lessThan(2000L))
+                .body("name", equalTo("pavan"))
+                .body("job", equalTo("trainer"))
+                .body(containsString("id"))
+                .log().all()
+                .extract().jsonPath().getInt("id");
+
+    }
+
+    //3.update the existing user and update
+    @Test(priority = 3)
+    void updateUser() {
+        HashMap<String, String> data = new HashMap<String, String>();
+        data.put("name", "Shobha");
+        data.put("job", "Engineer");
 
         given()
                 .contentType("application/json")
                 .body(data)
-        .when()
-                .post("https://reqres.in/api/users")
-        .then()
-                .statusCode(201)
-                .header("Content-Type",equalTo("application/json; charset=utf-8"))
+
+                .when()
+                .put("https://reqres.in/api/users/719")
+
+                .then()
+                .statusCode(200)
+                .header("Content-Type", equalTo("application/json; charset=utf-8"))
                 .time(lessThan(2000L))
-                .body("name",equalTo("pavan"))
-                .body("job",equalTo("trainer"))
-                .body(containsString("id"))
+                .body("name", equalTo("Shobha"))
+                .body("job", equalTo("Engineer"))
                 .log().all();
 
 
-
-
-
-
     }
+
 
 }
