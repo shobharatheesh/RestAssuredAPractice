0737be08773a69ba6931f244d0375424442bbe4c
Shobha Rao <sraork@gmail.com>
2025-04-14 00:03:39 +0530

    HTTP methods--13th March 2025

--- src/test/java/com/udemy/April132025/AuthenticationTests.java
+++ src/test/java/com/udemy/April132025/AuthenticationTests.java
@@ -11,21 +11,38 @@ public class AuthenticationTests {
 
     @Test
 
-    void verifyBasicAuth()
+    void verifyBasicAuth() {
+        given()
+                .auth().basic("postman", "password")
+
+                .when()
+                .get("https://postman-echo.com/basic-auth")
+
+                .then()
+                .statusCode(200)
+                .body("authenticated", equalTo(true))
+                .log().body();
+
+    }
+
+   //2.Basic preemptive Authentication
+    @Test
+    void verifyBasicPreemptiveAuth()
     {
         given()
-                .auth().basic("postman","password")
+
+                .auth().preemptive().basic("postman","password")
 
         .when()
                 .get("https://postman-echo.com/basic-auth")
 
         .then()
                 .statusCode(200)
-                .body("authenticated",equalTo(true))
+                .body("authenticated", equalTo(true))
                 .log().body();
 
-
-
-
     }
+
+
+
 }
